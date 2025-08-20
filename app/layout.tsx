263a1fac8eb081cfa03221dde482dbfc8a2c0b5a
mayo829 <mayo@yo>
2025-08-20 12:47:21 -0400

    build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 // import { Geist, Geist_Mono } from 'next/font/google';
-import { Share_Tech_Mono, JetBrains_Mono, Inter } from 'next/font/google';
+import { JetBrains_Mono, Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/next';
 
 import localFont from 'next/font/local';
